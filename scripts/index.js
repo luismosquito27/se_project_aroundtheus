b7c67a9e6308f8775577d8a3e80a1c5a266cffba
Luis Mosqueda <swpluis@gmail.com>
2023-05-21 23:40:42 -0500

    commit

---
+++ scripts/index.js
@@ -0,0 +1,40 @@
+const cardData1 = {
+  title: "Yosemite Valley",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+};
+
+const cardData2 = {
+  title: "Lake Louise",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+};
+
+const cardData3 = {
+  title: "Bald Mountains",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+};
+
+const cardData4 = {
+  title: "Latemar",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+};
+
+const cardData5 = {
+  title: "Vanoise National Park",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+};
+
+const cardData6 = {
+  title: "Lago di Braies",
+  link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg ",
+};
+
+const initialCards = {
+  cardData1,
+  cardData2,
+  cardData3,
+  cardData4,
+  cardData5,
+  cardData6,
+};
+
+console.log(initialCards);
