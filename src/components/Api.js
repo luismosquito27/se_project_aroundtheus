0eaf2723c2df66e369b4760952cc2e11fe6f11f9
Luis Mosqueda <swpluis@gmail.com>
2025-06-26 23:47:01 -0500

    commit

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,10 +1,35 @@
 class Api {
-    constructor(settings) { 
-        this.settings = settings; 
-    }
-    
+  getInitialCards() {
+    return fetch(" https://around-api.en.tripleten-services.com/v1/cards", {
+      method: "GET",
+      headers: {
+        authorization: "c56e30dc-2883-4270-a59e-b2f7bae969c6",
+        "content-Type": "application/json",
+      },
+    });
+
+api.getInitialCards() 
+ .then(cardData => { 
+    const card = createCard(cardData); 
+     const cardElement = createCard(cardData);
+      cardSection.append(cardElement);
+ })
+ .catch(err => console.error('Error fetching cards:', err));
 }
 
-getInitialCards() { 
+  }
 
-}
+
+
+
+//  .then(res => {
+//    if (res.ok) {
+//      return res.json();
+//    }
+// // if the server returns an error, reject the promise
+//        return Promise.reject(`Error: ${res.status}`);
+//      });
+//  }
+
+// GET https://around-api.en.tripleten-services.com/v1/users/me
+// Remember to pass the token in the request. If the request is successful, you will receive a user object in the response:
--- src/pages/index.js
+++ src/pages/index.js
@@ -156,4 +156,11 @@ const api = new Api({
     authorization: "c56e30dc-2883-4270-a59e-b2f7bae969c6",
     "Content-Type": "application/json",
   },
+}).then((res) => {
+  if (res.ok) {
+    return res.json();
+  }
 });
+
+// don't forget to run "npm run dev" in the terminal to
+//check your website.
